df558479b58be7d3e294299b253c76149e2fe181
jishant07 <jishanta@gmail.com>
2022-04-09 21:08:41 -0400

    Added Admin Check

--- functions/routes/appRoutes.js
+++ functions/routes/appRoutes.js
@@ -1,9 +1,10 @@
 const appRouter = require('express').Router();
 const appController = require('../controllers/appController')
+const adminMiddleWare = require('../middlewares/adminMiddleWare')
 
-appRouter.route("/addCountry").post(appController.addCountry)
-appRouter.route("/addPortToCountry").post(appController.addPortToCountry)
-appRouter.route("/addAirportToCountry").post(appController.addAirportToCountry)
+appRouter.route("/addCountry").post(adminMiddleWare,appController.addCountry)
+appRouter.route("/addPortToCountry").post(adminMiddleWare,appController.addPortToCountry)
+appRouter.route("/addAirportToCountry").post(adminMiddleWare,appController.addAirportToCountry)
 appRouter.route("/getPortsByCountry").post(appController.getPorts)
 appRouter.route("/getAirportsByCountry").post(appController.getAirports)
 appRouter.route("/listCountries").get(appController.listCountries)
